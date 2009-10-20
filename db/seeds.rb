9c82689c6153472bb514ce691e7f628f8412d77d
Ary Borenszweig <aborenszweig@manas.com.ar>
2009-10-20 17:42:29 +0200

    Added seed data for database: an application and a channel.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+require 'digest/sha2'
+
+app_salt = ActiveSupport::SecureRandom.base64(8)
+app_pass = Digest::SHA2.hexdigest(app_salt + 'riffpass')
+app = Application.create({ :name => 'riff', :salt => app_salt, :password => app_pass })
+
+chan_salt = ActiveSupport::SecureRandom.base64(8)
+chan_pass = Digest::SHA2.hexdigest(chan_salt + 'smspass')
+Channel.create(:name => 'sms', :kind => :qst, :configuration => { :salt => chan_salt, :password => chan_pass }, :application_id => app.id)
