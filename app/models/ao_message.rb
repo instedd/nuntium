2436c6c7314f21ba1fbe331ed921cb016490a0b1
Ary Borenszweig <aborenszweig@manas.com.ar>
2009-10-27 16:16:04 +0100

    Fixed subject_and_body didn't work when subject wasn't empty but body was.

--- app/models/ao_message.rb
+++ app/models/ao_message.rb
@@ -1,4 +1,20 @@
 class AOMessage < ActiveRecord::Base
   belongs_to :application
   validates_presence_of :application
+  
+  def subject_and_body
+    if self.subject.nil? || self.subject == ''
+      if self.body.nil? || self.body == ''
+        ''
+      else
+        self.body
+      end
+    else
+      if self.body.nil? || self.body == ''
+        self.subject
+      else
+        self.subject + ' - ' + self.body
+      end
+    end
+  end
 end
