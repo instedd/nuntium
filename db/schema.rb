1ca6cab4e14b338b200a6f6860150d00381c864b
Ary Borenszweig <aborenszweig@manas.com.ar>
2009-10-30 00:04:29 +0100

    Added delayed_job dependency.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091023140800) do
+ActiveRecord::Schema.define(:version => 20091029190309) do
 
   create_table "ao_messages", :force => true do |t|
     t.string   "from"
@@ -59,6 +59,19 @@ ActiveRecord::Schema.define(:version => 20091023140800) do
     t.string   "configuration"
     t.string   "salt"
     t.string   "protocol"
+  end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "qst_outgoing_messages", :force => true do |t|
