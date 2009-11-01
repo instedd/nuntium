2183145a8e7ce191763d3fb6adfd332144844769
Ary Borenszweig <aborenszweig@manas.com.ar>
2009-11-01 12:27:43 +0100

    Missing 'require'.

--- app/models/channel_handlers/clickatell.rb
+++ app/models/channel_handlers/clickatell.rb
@@ -1,3 +1,6 @@
+require 'uri'
+require 'net/http'
+
 class ClickatellChannelHandler < ChannelHandler
   def handle(msg)
     Delayed::Job.enqueue SendClickatellMessageJob.new(@channel.application_id, @channel.id, msg.id)
