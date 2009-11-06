475adabda92407c016711ce784d3de47b660cdf7
Ary Borenszweig <aborenszweig@manas.com.ar>
2009-11-06 19:23:29 +0100

    Fixed formatting of log messages.

--- app/models/application_logger.rb
+++ app/models/application_logger.rb
@@ -6,15 +6,15 @@ class ApplicationLogger < Logger
   end
   
   def protocol_not_found_for(msg)
-    self.warn '[POST /rss] Protocol not found for #{msg.inspect}'
+    self.warn "[POST /rss] Protocol not found for #{msg.inspect}"
   end
   
   def no_channel_found_for(protocol, msg)
-    self.warn '[POST /rss] No channel found for protocol "#{protocol}" for message "#{msg.inspect}"'
+    self.warn "[POST /rss] No channel found for protocol '#{protocol}' for message #{msg.inspect}"
   end
   
   def more_than_one_channel_found_for(protocol, msg)
-    app_logger.warn '[POST /rss] More than one channel found for protocol "#{protocol} for message "#{msg.inspect}"'
+    app_logger.warn "[POST /rss] More than one channel found for protocol '#{protocol}' for message #{msg.inspect}"
   end
   
   def format_message(severity, timestamp, progname, msg)
