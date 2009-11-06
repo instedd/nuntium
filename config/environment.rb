1240711340bd3b88a625aca8de9841910abe8b9b
Ary Borenszweig <aborenszweig@manas.com.ar>
2009-11-06 15:27:16 +0100

    Added sqlite3-ruby gem dependency.

--- config/environment.rb
+++ config/environment.rb
@@ -17,8 +17,8 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem 'collectiveidea-delayed_job', :lib => 'delayed_job', :source => 'http://gems.github.com'
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
   config.gem "tmail"
