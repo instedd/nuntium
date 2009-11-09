b018f2360064f14687b240c591029adb62dd6943
Ary Borenszweig <aborenszweig@manas.com.ar>
2009-11-09 18:41:04 +0100

    Simplified js.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,18 @@
-function delete_channel(id, name) {
-  if (confirm('Are you sure you want to delete the channel ' + name + '?')) {
-    window.location = '/channel/delete/' + id;
-  }
+function $(id) {
+  return document.getElementById(id);
 }
 
-function toggle_visibility(id) {
-  var elem = document.getElementById(id);
+function toggle(id) {
+  var elem = $(id);
   if (elem.style.display != 'none') {
     elem.style.display = 'none';
   } else {
     elem.style.display = '';
+  }
+}
+
+function delete_channel(id, name) {
+  if (confirm('Are you sure you want to delete the channel ' + name + '?')) {
+    window.location = '/channel/delete/' + id;
   }
 }
