923bb9db09a91e94b698de488927e4680b59de2f
nditada@nicolas-di-tadas-macbook-pro.local <nditada@nicolas-di-tadas-macbook-pro.local>
2009-12-08 15:56:25 +0700

    Hacking of DTAC post of AO. Configuration hardcoded.

--- app/models/dtac/send_dtac_message_job.rb
+++ app/models/dtac/send_dtac_message_job.rb
@@ -8,8 +8,43 @@ class SendDtacMessageJob
   end
 
   def perform
-    # TODO: Send the message to dtac
-    true
+    msg = AOMessage.find @message_id
+    
+    host = URI::parse('https://corpsms.dtac.co.th/servlet/com.iess.socket.SmsCorplink')
+    
+    request = Net::HTTP::Post.new(host.path)
+    request.use_ssl = true
+    request.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+	#request.basic_auth 'api1610368', 'u41jjmew'
+    request.set_form_data({
+					'RefNo'=>msg.guid, 
+					'Msn'=>msg.to.without_protocol,
+					'Sno'=>'1677',
+					'Msg'=>msg.subject_and_body,
+					'Encoding'=>245,
+					'MsgType'=>'E',
+					'User' => 'api1610368',
+					'Password' => 'u41jjmew'
+				}, ';')
+	
+    response = Net::HTTP.new(url.host, url.port).start {|http| http.request(request) }
+
+    result = ''
+    begin
+      result = response.body[4 ... response.body.length]
+    rescue => e
+      ApplicationLogger.exception_in_channel_and_ao_message channel, msg, e
+      msg.tries += 1;
+      msg.save
+      raise
+    else    
+      msg.state = 'delivered'
+      msg.tries += 1
+      msg.save
+    end
+    
+    result
   end
   
 end
