95b701532a9f7ce67ec8981ea7bee832c4601daa
spalladino@gmail.com <spalladino@gmail.com>
2009-12-11 22:49:26 -0800

    Dtac thai encoding issues

--- app/controllers/dtac_controller.rb
+++ app/controllers/dtac_controller.rb
@@ -1,12 +1,24 @@
 class DtacController < ApplicationController
   before_filter :authenticate
-
+	require 'iconv'
+  
   def index  
-	  msg = ATMessage.new
+	File.open('c:\dtac.log', 'a'){ 
+		|fh|  
+		fh.puts 'Received new AT message'
+		params.each { |k,v| 
+			fh.puts(' ' + k.to_s + ': ' + v.to_s) 
+		} 
+	}
+	
+	converter = Iconv.new('UTF-8','TIS-620')
+	text = converter.iconv(params[:CONTENT])
+	
+	msg = ATMessage.new
     msg.application_id = @application.id
     msg.from = 'sms://' + params[:MSISDN]
     msg.to = 'sms://' + params[:SMSCODE]
-    msg.subject = params[:CONTENT]
+    msg.body = text
     msg.guid = params[:ID]
     msg.timestamp = Time.now.utc
     msg.state = 'queued'
--- app/models/dtac/send_dtac_message_job.rb
+++ app/models/dtac/send_dtac_message_job.rb
@@ -1,6 +1,9 @@
 class SendDtacMessageJob
   attr_accessor :application_id, :channel_id, :message_id
 
+	require 'iconv'
+	
+  
   def initialize(application_id, channel_id, message_id)
     @application_id = application_id
     @channel_id = channel_id
@@ -13,15 +16,26 @@ class SendDtacMessageJob
     return if msg.nil? or channel.nil?
 	config = channel.configuration
 	
+	str = msg.subject_and_body
+	encoded = ActiveSupport::Multibyte::Chars.u_unpack(str).map { |i| i.to_s(16).rjust(4, '0') }
+	
+	File.open('c:\dtac.log', 'a'){ 
+		|fh|  
+		fh.puts 'Sending new AO message'
+		fh.puts ' ' + msg.subject_and_body
+		fh.puts ' ' + encoded.to_s
+	}
+	 
+	
 	response = Net::HTTP.post_form(
 		URI.parse('http://corpsms.dtac.co.th/servlet/com.iess.socket.SmsCorplink'), {
 			'RefNo'=>(0...14).map{ ('a'..'z').to_a[rand(26)] }.join, #HACK: DTAC supports only 15 chars for ID, we need to figure out what to use
 			'Msn'=>msg.to.without_protocol,
 			'Sno'=>config[:sno],
 			'Sender'=>config[:sno],
-			'Msg'=>msg.subject_and_body,
-			'Encoding'=>245,
-			'MsgType'=>'E',
+			'Msg'=>encoded.to_s,
+			'Encoding'=>24,
+			'MsgType'=>'H',
 			'User' =>  config[:user],
 			'Password' => config[:password]})
 				
