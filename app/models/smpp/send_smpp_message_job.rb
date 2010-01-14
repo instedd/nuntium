1ea226c0fb10174533949e06b8ca0985e745fb34
medinasergio@gmail.com <medinasergio@gmail.com>
2010-01-14 15:13:25 +0700

    added logging to send smpp message job

--- app/models/smpp/send_smpp_message_job.rb
+++ app/models/smpp/send_smpp_message_job.rb
@@ -10,8 +10,14 @@ class SendSmppMessageJob
   end
   
   def perform
-
+    
+    # fetch process associated with this channel
     @d_rb_process = DRbProcess.find_by_channel_id @channel_id
+    if @d_rb_process.nil?
+      RAILS_DEFAULT_LOGGER.error "Couldn't find registered DRb service for channel with id #{@channel_id}."
+      return :error_finding_drb_service
+    end
+    
     msg = AOMessage.find @message_id
     
     # should we put body, subject or both here?
@@ -24,6 +30,7 @@ class SendSmppMessageJob
       @smpp_gw = DRbObject.new nil, @d_rb_process.uri
   
       #@smpp_gw.send_msg(@message_id)
+      RAILS_DEFAULT_LOGGER.debug "Sending AOMessage with id #{@message_id} through SMPP channel with id #{@channel_id}."
       @smpp_gw.send_message(from, to, msg.body)
     rescue => e
       ApplicationLogger.exception_in_channel_and_ao_message channel, msg, e
