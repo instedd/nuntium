dc7aa74f9ff3d780335d01fd9d8230f0d19d6a7d
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-02-09 16:55:34 -0300

    Documented that "app" is available in alert script.

--- app/models/alerts/alert_interpreter.rb
+++ app/models/alerts/alert_interpreter.rb
@@ -1,11 +1,11 @@
 class AlertInterpreter
 
-  def interpret_for(application)
-    return if application.configuration[:alert].nil?
+  def interpret_for(app)
+    return if app.configuration[:alert].nil?
   
-    trigger = AlertTrigger.new application
+    trigger = AlertTrigger.new app
     begin
-      eval application.configuration[:alert]
+      eval app.configuration[:alert]
     rescue Exception => e
       trigger.alert 'system_internal', fix_error("You have an error in your alert code: #{e.message}")
     end
