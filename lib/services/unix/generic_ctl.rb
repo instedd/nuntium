01695c33ce164e4554848b22e5b87ecd6b5b6c06
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-03-05 17:04:14 -0300

    Fixed starting wrong script.

--- lib/services/unix/generic_ctl.rb
+++ lib/services/unix/generic_ctl.rb
@@ -2,7 +2,7 @@ def run(script, id = nil)
   require 'rubygems'
   require 'daemons'
   app_name = script
-  app_name << ".#{id}" if id 
+  app_name += ".#{id}" if id 
   Daemons.run(
     File.join(File.dirname(__FILE__), "#{script}.rb"),
     :app_name => app_name,
