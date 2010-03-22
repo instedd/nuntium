952f0a8868da75dfd40fce9fc84a6805309241ea
Juan Wajnerman <juan.wajnerman@gmail.com>
2010-03-22 13:09:39 -0300

    Started refactor of the transceiver delegate
    --HG--
    branch : smpp-refactor

---
+++ app/models/smpp/smpp_transceiver_delegate.rb
@@ -0,0 +1,75 @@
+require 'iconv'
+
+class SmppTransceiverDelegate
+  
+  EncodingCode = { "ascii" => 1, "latin1" => 3, "ucs-2be" => 8, "ucs-2le" => 8 }
+  
+  def initialize(transceiver, channel)
+    @transceiver = transceiver
+    @channel = channel
+    @encodings = @channel.configuration[:mt_encodings].map { |x| encoding_endianized x }
+  end
+  
+  def send_message(id, from, to, text)
+    
+    msg_text = nil
+    msg_coding = nil
+    
+    # Select best encoding for the message
+    @encodings.each do |encoding|
+      iconv = Iconv.new(encoding, 'utf-8')
+      msg_text = iconv.iconv(text) rescue next
+      msg_coding = EncodingCode[encoding]
+      break
+    end
+    
+    @transceiver.send_mt(id, from, to, msg_text, {:data_coding => msg_coding})
+  end
+  
+  def mo_received(transceiver, pdu)
+    msg = ATMessage.new
+    msg.from = pdu.source_addr.with_protocol 'sms'
+    msg.to = pdu.destination_addr.with_protocol 'sms'
+    if @channel.configuration[:accept_mo_hex_string] and is_hex(pdu.short_message) 
+      bytes = hex_to_bytes pdu.short_message
+      iconv = Iconv.new('utf-8', ucs2_endianized)
+      msg.subject = iconv.iconv bytes
+    else
+      source_encoding = case pdu.data_coding
+        when 0: encoding_endianized(@channel.configuration[:default_mo_encoding])
+        when 1: 'ascii'
+        when 3: 'latin1'
+        when 8: ucs2_endianized
+      end
+      
+      if source_encoding
+        iconv = Iconv.new('utf-8', source_encoding)
+        msg.subject = iconv.iconv pdu.short_message
+      else
+        msg.subject = pdu.short_message
+      end
+    end
+    
+    @channel.accept msg
+    
+  end
+  
+  private
+  
+  def encoding_endianized(encoding)
+    encoding == 'ucs-2' ? ucs2_endianized : encoding
+  end
+  
+  def ucs2_endianized
+    @channel.configuration[:endianness] == :little ? 'ucs-2le' : 'ucs-2be'
+  end
+  
+  def is_hex(msg)
+    msg =~ /[0-9a-fA-F]{4}+/
+  end
+  
+  def hex_to_bytes(msg)
+    msg.scan(/../).map{|x| x.to_i(16).chr}.join
+  end
+  
+end
---
+++ test/unit/smpp_tranceiver_delegate_test.rb
@@ -0,0 +1,126 @@
+require 'test_helper'
+require 'mocha'
+require 'smpp'
+
+class SmppTranceiverDelegateTest < ActiveSupport::TestCase
+  include Mocha::API
+  
+  def setup
+    @application = Application.create!(:name => "testapp", :password => "testpass")
+    @chan = Channel.new(:application_id => @application.id, :name => 'chan', :kind => 'smpp', :protocol => 'sms')
+    @chan.configuration = {:host => 'host', :port => '3200', :source_ton => 0, :source_npi => 0, :destination_ton => 0, :destination_npi => 0, :user => 'user', :password => 'password', :encoding => 'utf16le', :system_type => 'smpp' }
+    @transceiver = mock('Smpp::Transceiver')
+  end
+
+  def send_message(encodings, input, output, output_coding, endianness = :big)
+    @chan.configuration[:mt_encodings] = encodings
+    @chan.configuration[:endianness] = endianness
+    @chan.save!
+    
+    @transceiver.expects(:send_mt).with(123, '8888', '4444', output, { :data_coding => output_coding })
+    
+    @delegate = SmppTransceiverDelegate.new(@transceiver, @chan)
+    @delegate.send_message(123, '8888', '4444', input)
+  end
+  
+  def receive_message(input, input_coding, output, endianness = :big, default_mo_encoding = 'ascii', accept_mo_hex_string = false)
+    @chan.configuration[:mt_encodings] = []
+    @chan.configuration[:endianness] = endianness
+    @chan.configuration[:default_mo_encoding] = default_mo_encoding
+    @chan.configuration[:accept_mo_hex_string] = accept_mo_hex_string
+    @chan.save!
+    @delegate = SmppTransceiverDelegate.new(@transceiver, @chan)
+    
+    pdu = Smpp::Pdu::DeliverSm.new '4444', '8888', input, :data_coding => input_coding
+    @delegate.mo_received @transceiver, pdu
+    msgs = ATMessage.all
+    assert_equal 1, msgs.length
+    msg = msgs[0]
+    assert_equal 'sms://4444', msg.from
+    assert_equal 'sms://8888', msg.to
+    assert_equal output, msg.subject
+    assert_equal @chan.id, msg.channel_id
+  end
+
+  test "send ascii message" do
+    send_message ['ascii'], 'Hello', 'Hello', 1
+  end
+  
+  test "send ascii message as latin1" do
+    send_message ['latin1'], 'Hello', 'Hello', 3
+  end
+  
+  test "send ascii message as ascii" do
+    send_message ['ascii', 'latin1'], 'Hello', 'Hello', 1
+  end
+  
+  test "send latin1 message as latin1" do
+    send_message ['ascii', 'latin1'], 'árbol', "\341rbol", 3
+  end
+  
+  test "send ascii message as ucs2" do
+    send_message ['ucs-2'], 'hola', "\000h\000o\000l\000a", 8
+  end
+  
+  test "send unicode message as ucs2" do    
+    send_message ['ascii', 'latin1', 'ucs-2'], "你好", "\117\140\131\175", 8
+  end
+  
+  test "send ucs2 little endian" do
+    send_message ['ucs-2'], 'hola', "h\000o\000l\000a\000", 8, :little
+  end
+  
+  test "receive ascii message" do
+    receive_message 'hello', 1, 'hello'
+  end
+  
+  test "receive latin1 message" do
+    receive_message "\341rbol", 3, 'árbol'
+  end
+  
+  test "receive ucs2 message" do
+    receive_message "\000h\000o\000l\000a", 8, 'hola'
+  end
+  
+  test "receive usc2-le message" do
+    receive_message "h\000o\000l\000a\000", 8, 'hola', :little
+  end
+  
+  test "receive default encoding message" do
+    receive_message "h\000o\000l\000a\000", 0, 'hola', :little, 'ucs-2'
+  end
+  
+  test "receive hex string" do
+    receive_message "006100620063", 0, 'abc', :big, 'ascii', true
+  end
+  
+  test "receive hex string not hex" do
+    receive_message "h\000o\000l\000a\000", 8, 'hola', :little, 'ascii', true
+  end
+  
+  test "receive lao message" do
+    pdubin = '0000003d000000050000000000002a6f00000038353632303232313032383900000032343838000000000000000000000c306538313065383230653834'
+    pdu = Smpp::Pdu::Base.create(pdubin.scan(/../).map{|x| x.to_i(16).chr}.join)
+    
+    @chan.configuration[:mt_encodings] = []
+    @chan.configuration[:endianness] = :big
+    @chan.configuration[:default_mo_encoding] = 'ascii'
+    @chan.configuration[:accept_mo_hex_string] = true
+    @chan.save!
+    @delegate = SmppTransceiverDelegate.new(@transceiver, @chan)
+    
+    @delegate.mo_received @transceiver, pdu
+    msgs = ATMessage.all
+    assert_equal 1, msgs.length
+    msg = msgs[0]
+    assert_equal 'sms://856202210289', msg.from
+    assert_equal 'sms://2488', msg.to
+    assert_equal "ກຂຄ", msg.subject
+    assert_equal @chan.id, msg.channel_id 
+  end
+  
+  test "receive unkonwn encoding" do
+    receive_message "h\000o\000l\000a\000", 7, "h\000o\000l\000a\000"
+  end
+  
+end
