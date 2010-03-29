2a3c4e7d0be31ad2a8497f16a07578c2f817d49c
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-03-29 15:28:53 -0300

    Added ampq gem dependency.
    --HG--
    branch : smpp-rabbit

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "guid"
   config.gem 'twitter', :version => '0.6.15'  
   config.gem 'ruby-smpp', :lib => 'smpp', :version => '0.2.1'
+  config.gem 'amqp'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
