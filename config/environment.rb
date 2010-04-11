9a0224d677726861ef31e9801ef9f69a9e377da0
asterite@localhost.localdomain <asterite@localhost.localdomain>
2010-04-11 14:12:29 -0300

    Removed sqlite dependency.
    --HG--
    branch : smpp-rabbit

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "mocha"
-  config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem 'collectiveidea-delayed_job', :lib => 'delayed_job', :source => 'http://gems.github.com'
   config.gem 'test-unit', :lib => 'test/unit'
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
