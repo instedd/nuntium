ded50dc5e94f7780a5f31ee6e76cb6ad62020b31
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-04-12 15:16:26 -0300

    More exceptions handled in twtitter channel.
    --HG--
    branch : smpp-rabbit

--- app/models/twitter/send_twitter_message_job.rb
+++ app/models/twitter/send_twitter_message_job.rb
@@ -4,6 +4,8 @@ class SendTwitterMessageJob < SendMessageJob
     response = client.direct_message_create(@msg.to.without_protocol, @msg.subject_and_body)
     @msg.channel_relative_id = response.id
     @msg.send_succeeed @app, @channel
+  rescue Twitter::NotFound => ex
+    raise MessageException.new(ex)
   rescue Twitter::Unauthorized => ex
     raise PermanentException.new(ex)
   end
