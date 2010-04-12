2f756030de5728a0b903149d5e0a78ef6da7a0d1
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-04-12 14:45:00 -0300

    Better handling of twitter exceptions.
    --HG--
    branch : smpp-rabbit

--- app/models/twitter/send_twitter_message_job.rb
+++ app/models/twitter/send_twitter_message_job.rb
@@ -1,13 +1,10 @@
 class SendTwitterMessageJob < SendMessageJob
   def managed_perform
-    begin
-      client = TwitterChannelHandler.new_client(@config)
-      response = client.direct_message_create(@msg.to.without_protocol, @msg.subject_and_body)
-      @msg.channel_relative_id = response.id
-    rescue => e
-      @msg.send_failed @app, @channel, e
-    else
-      @msg.send_succeeed @app, @channel
-    end
+    client = TwitterChannelHandler.new_client(@config)
+    response = client.direct_message_create(@msg.to.without_protocol, @msg.subject_and_body)
+    @msg.channel_relative_id = response.id
+    @msg.send_succeeed @app, @channel
+  rescue Twitter::Unauthorized => ex
+    raise PermanentException.new(ex)
   end
 end
