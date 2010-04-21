2cd6dfd4a16ef4587cda289f6b8a2c3b6edecdab
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-04-21 17:06:33 -0300

    Small refactor.
    --HG--
    branch : routing-refactor

--- app/controllers/api/country_controller.rb
+++ app/controllers/api/country_controller.rb
@@ -3,12 +3,18 @@ class CountryController < ApplicationController
   def index
     case params[:format]
     when 'xml'
-      render :xml => Country.all.to_xml
+      render :xml => countries.to_xml
     when 'json'
-      render :json => Country.all.to_json(:except => [:id, :created_at, :updated_at])
+      render :json => countries.to_json(:except => [:id, :created_at, :updated_at])
     else
       head :bad_request
     end
+  end
+  
+  private
+  
+  def countries
+    Country.all
   end
 
 end
