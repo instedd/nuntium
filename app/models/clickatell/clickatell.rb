828dfd78c819bd9f12719914556db5c2a09826e7
Juan Wajnerman <juan.wajnerman@gmail.com>
2010-06-18 11:58:35 -0300

    Clickatell API always uses HTTPS

--- app/models/clickatell/clickatell.rb
+++ app/models/clickatell/clickatell.rb
@@ -5,17 +5,17 @@ module Clickatell
   end
   
   def self.get_status(query_parameters)
-    Clickatell.get("/http/querymsg?#{query_parameters.to_query}", :protocol => 'https').body 
+    Clickatell.get("/http/querymsg?#{query_parameters.to_query}").body 
   end
 
   def self.send_message(query_parameters)
-    Clickatell.get "/http/sendmsg?#{query_parameters.to_query}", :protocol => 'https'
+    Clickatell.get "/http/sendmsg?#{query_parameters.to_query}"
   end
   
   private
   
-  def self.get(path, options = {:protocol => 'http'})
-    RestClient.get "#{options[:protocol]}://api.clickatell.com#{path}"
+  def self.get(path)
+    RestClient.get "https://api.clickatell.com#{path}"
   end
 
 end
