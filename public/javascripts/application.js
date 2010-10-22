4601db3a892a6d280e74165135dcd0bb120ee21b
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-10-22 16:09:31 -0300

    Now can filter channels by kind in UI

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -97,14 +97,14 @@ function mark_ao_messages_as_cancelled() {
     alert('No Application Originated messages were selected');
     return;
   }
-  
+
   if ($('#ao_all').val() == 1) {
     count = total_ao_messages;
   }
-  
+
   if (!confirm('' + count + ' Application Originated message' + (count == 1 ? '' : 's') + ' will be cancelled. Are you sure?'))
     return;
-  
+
   var form = document.getElementById('ao_messages_form');
   form.action = '/message/ao/mark_as_cancelled';
   form.submit();
@@ -116,14 +116,14 @@ function mark_at_messages_as_cancelled() {
     alert('No Application Terminated messages were selected');
     return;
   }
-  
+
   if ($('#at_all').val() == 1) {
     count = total_at_messages;
   }
-  
+
   if (!confirm('' + count + ' Application Terminated message' + (count == 1 ? '' : 's') + ' will be cancelled. Are you sure?'))
     return;
-  
+
   var form = document.getElementById('at_messages_form');
   form.action = '/message/at/mark_as_cancelled';
   form.submit();
@@ -135,14 +135,14 @@ function reroute_ao_messages() {
     alert('No Application Originated messages were selected');
     return;
   }
-  
+
   if ($('#ao_all').val() == 1) {
     count = total_ao_messages;
   }
-  
+
   if (!confirm('' + count + ' Application Originated message' + (count == 1 ? '' : 's') + ' will be re-routed. Are you sure?'))
     return;
-  
+
   var form = document.getElementById('ao_messages_form');
   form.action = '/message/ao/reroute';
   form.submit();
@@ -154,20 +154,36 @@ function create_channel(select) {
   select.value = '';
 }
 
+function filter_channels_by_kind(select) {
+  kind = select.value;
+  count = -1;
+  $('#channels tr').each(function() {
+      count++;
+      if (count < 2) return;
+      $this = $(this);
+      row_kind = $this.children().eq(1).text();
+      if (kind == '' || kind == row_kind) {
+        $this.show();
+      } else {
+        $this.hide();
+      }
+  });
+}
+
 // === clickatell ===
 
 function clickatell_channel_direction_changed() {
   var dir = $('#channel_direction :selected').val();
-  
+
   // incoming
   if (dir & 1) {
     show('incoming_password_container', 'callback_incoming');
   } else {
     hide('incoming_password_container', 'callback_incoming');
   }
-  
+
   // outgoing
-  if (dir & 2) { 
+  if (dir & 2) {
     show('user_container', 'password_container', 'from_container', 'callback_ack');
   } else {
     hide('user_container', 'password_container', 'from_container', 'callback_ack');
@@ -177,7 +193,7 @@ function clickatell_channel_direction_changed() {
 function clickatell_view_credit(id) {
   $.ajax({
     type: "GET",
-    url: '/clickatell/view_credit', 
+    url: '/clickatell/view_credit',
     data: {id: id},
     success: function(data) {
       alert(data)
@@ -193,7 +209,7 @@ function clickatell_view_credit(id) {
 function twitter_view_rate_limit_status(id) {
   $.ajax({
     type: "GET",
-    url: '/twitter/view_rate_limit_status', 
+    url: '/twitter/view_rate_limit_status',
     data: {id: id},
     success: function(data) {
       alert(data)
@@ -210,13 +226,13 @@ function channel_custom_attribute_changed(select) {
   select = $(select);
   li = select.parent();
   first_select = $(li.children()[0]);
-  first_select.attr('name', 'custom_attribute_name[]'); 
-  
+  first_select.attr('name', 'custom_attribute_name[]');
+
   li_value = li.children('.value');
-  
+
   cloned_li = li.clone();
   $(cloned_li.children()[0]).val('');
-  
+
   value = select.val();
   switch(value) {
   case '':
@@ -267,18 +283,18 @@ function channel_custom_attribute_changed(select) {
     break;
   case 'custom':
     first_select.attr('name', 'doesnt_matter');
-    
+
     var html = 'Name: <input type="text" name="custom_attribute_name[]"> Value: <input type="text" name="custom_attribute_value[]">';
     if (show_accept_when_not_specified_option) html += ' &nbsp; ' + accept_when_not_specified_checkbox();
     html += ' &nbsp; ' + remove_custom_attribute_link();
     li_value.html(html);
     break;
   }
-  
+
   ul = li.parent();
   lis = ul.children();
   last_li = lis[lis.length - 1];
-  
+
   if ($($(last_li).children()[0]).val()) {
     $("#custom_attributes").append(cloned_li);
   }
@@ -289,12 +305,12 @@ function channel_custom_attribute_country_changed(select) {
   country = select.val();
   li = select.parent();
   li_value2 = li.children('.value2');
-  
+
   if (!country) {
     li_value2.html('');
     return;
   }
-  
+
   get_carriers(country, {
     success: function(carriers) {
       li_value2.html(carriers_select(carriers, 'custom_attribute_value[]'));
@@ -318,7 +334,7 @@ function get_countries(map) {
   $.ajax({
     type: "GET",
     url: '/api/countries.json',
-    dataType: 'json', 
+    dataType: 'json',
     success: map.success,
     error: map.error,
   });
@@ -328,7 +344,7 @@ function get_carriers(country_id, map) {
   $.ajax({
     type: "GET",
     url: '/api/carriers.json?country_id=' + country_id,
-    dataType: 'json', 
+    dataType: 'json',
     success: map.success,
     error: map.error,
   });
@@ -338,7 +354,7 @@ function get_carrier(carrier_id, map) {
   $.ajax({
     type: "GET",
     url: '/api/carriers/' + carrier_id + '.json',
-    dataType: 'json', 
+    dataType: 'json',
     success: map.success,
     error: map.error,
   });
@@ -405,20 +421,20 @@ function rules_newId() { rules_nextId++; return rules_nextId; }
 
 function add_rule_ui(ctx, prefix, rule, matchings, actions) {
 	var table = jQuery('table', ctx);
-	
+
 	var rule_id = rules_newId();
 	var rule_prefix = prefix + '[' + rule_id + ']'
-	
+
 	var row = jQuery('<tr><td><a href="#" class="remove-rule">[x]</a></td><td><a href="#" class="add-matching">add condition</a></td><td><a href="#" class="add-action">add action</a></td><td><input type="checkbox" name="' + rule_prefix +'[stop]" value="yes"></td></tr>');
 	table.append(row);
 	var add_matching = jQuery('.add-matching', row);
 	var add_action = jQuery('.add-action', row);
-	
+
 	jQuery('.remove-rule', row).click(function(){ row.remove(); return false; });
 	add_matching.click(function(){ add_matching_ui(rule_id, add_matching, rule_prefix, null, matchings); return false; });
 	add_action.click(function(){  add_action_ui(rule_id, add_action, rule_prefix, null, actions); return false; });
-	
-	if (rule != null) {		
+
+	if (rule != null) {
 		// load existing matchings
 		jQuery(rule.matchings).each(function(_, matching){
 			add_matching_ui(rule_id, add_matching, rule_prefix, matching, matchings);
@@ -439,30 +455,30 @@ function add_matching_ui(rule_id, add_matching, prefix, matching, matchings) {
 	var matching_id = rules_newId();
 	var matching_ui = jQuery('<div/>');
 	add_matching.before(matching_ui);
-	
+
 	// fill matching ui
 	if (!matchings) {
-	  matchings = ['application', 'body', 'country', 'carrier', 'from', 'subject', 'subject_and_body', 'to', 'other']; 
+	  matchings = ['application', 'body', 'country', 'carrier', 'from', 'subject', 'subject_and_body', 'to', 'other'];
 	}
-	
+
 	var name_prefix = prefix + '[matchings][' + matching_id + ']';
 	var matching_ui_str = '';
 	matching_ui_str += '<span class="property"><select name="' + name_prefix +'[property]">';
 	matching_ui_str += property_combo_string(matchings);
 	matching_ui_str += '</select></span>';
-	
+
 	matching_ui.append(matching_ui_str);
 	matching_ui.append('<select class="operator" name="' + name_prefix +'[operator]"><option value="equals">equals</option><option value="not_equals">not equals</option><option value="starts_with">starts with</option><option value="regex">regex</option></select>');
 	matching_ui.append('<span class="value"><input type="text" name="' + name_prefix +'[value]"/></span>');
 	matching_ui.append('<a href="#" class="remove-matching">[x]</a>');
-	
+
 	jQuery('.remove-matching', matching_ui).click(function(){ matching_ui.remove(); return false; });
-	
+
 	var property = jQuery('select:first', matching_ui);
 	var propertyDiv = jQuery('.property', matching_ui);
 	var valueDiv = jQuery('.value', matching_ui);
 	var operatorSelect = jQuery('.operator', matching_ui);
-	
+
 	init_properties(name_prefix, property, propertyDiv, valueDiv, operatorSelect);
 	if (matching != null) {
 	  init_existing_property(matching, name_prefix, property, propertyDiv, valueDiv, operatorSelect);
@@ -474,29 +490,29 @@ function add_action_ui(rule_id, add_action, prefix, action, actions) {
 	var action_id = rules_newId();
 	var action_ui = jQuery('<div/>');
 	add_action.before(action_ui);
-	
+
 	// fill action ui
 	if (!actions) {
 	  actions = ['application', 'body', 'country', 'carrier', 'from', 'subject', 'to', 'other'];
 	}
-	
+
 	var name_prefix = prefix + '[actions][' + action_id + ']';
 	var action_ui_str = '';
 	action_ui_str += '<span class="property"><select name="' + name_prefix +'[property]">';
 	action_ui_str += property_combo_string(actions);
 	action_ui_str += '</span>';
-	
+
 	action_ui.append(action_ui_str);
 	action_ui.append(' = ');
 	action_ui.append('<span class="value"><input type="text" name="' + name_prefix +'[value]"/></span>');
 	action_ui.append('<a href="#" class="remove-action">[x]</a>');
-	
+
 	jQuery('.remove-action', action_ui).click(function(){ action_ui.remove(); return false; });
-	
+
 	var property = jQuery('select:first', action_ui);
 	var propertyDiv = jQuery('.property', action_ui);
 	var valueDiv = jQuery('.value', action_ui);
-	
+
 	init_properties(name_prefix, property, propertyDiv, valueDiv);
 	if (action != null) {
 	  init_existing_property(action, name_prefix, property, propertyDiv, valueDiv);
@@ -507,12 +523,12 @@ function init_rules(ctx, prefix, rules, matchings, actions) {
 	// initial ui
 	ctx.append('<table class="table"><tr><th>&nbsp;</th><th>Condition</th><th>Action</th><th>Stop</th></tr></table>');
 	ctx.append('<div><a href="#" class="add-rule">add rule</a></div><br/>');
-		
+
 	jQuery('.add-rule', ctx).click(function(){
-		add_rule_ui(ctx, prefix, null, matchings, actions);				
+		add_rule_ui(ctx, prefix, null, matchings, actions);
 		return false;
 	});
-	
+
 	// load existing rules
 	if (rules != null) {
 		jQuery(rules).each(function(_, rule){
@@ -606,7 +622,7 @@ function init_property_carrier(name_prefix, valueDiv, operatorSelect, existing) 
           jQuery('select:last', valueDiv).remove();
         }
       };
-      
+
       countriesSelect.change(function() { countriesSelectChange() });
       if (existing) {
         get_carrier(existing.value, {
@@ -632,7 +648,7 @@ function init_property_other(name_prefix, propertyDiv, valueDiv, operatorSelect,
     operatorSelect.html(op_all());
   }
   valueDiv.html('<input type="text" name="' + name_prefix +'[value]"/>');
-  
+
   if (existing) {
     jQuery('input', propertyDiv).val(existing.property);
     jQuery('input', valueDiv).val(existing.value);
@@ -647,7 +663,7 @@ function init_property_field(name_prefix, propertyDiv, valueDiv, operatorSelect,
     operatorSelect.html(op_all());
   }
   valueDiv.html('<input type="text" name="' + name_prefix +'[value]"/>');
-  
+
   if (existing) {
     jQuery('input', propertyDiv).val(existing.property);
     jQuery('input', valueDiv).val(existing.value);
