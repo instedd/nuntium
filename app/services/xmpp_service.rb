974f6bd2c6ac224665491fa71d94c5fab5cd36b6
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-11-04 11:04:08 -0300

    Backtrace for exceptions in xmpp service

--- app/services/xmpp_service.rb
+++ app/services/xmpp_service.rb
@@ -10,55 +10,54 @@ class XmppService < Service
   def initialize(channel)
     @channel = channel
     c = @channel.configuration
-    
+
     jid_str = "#{c[:user]}@#{c[:domain]}"
     jid_str << "/#{c[:resource]}" unless c[:resource].blank?
     @jid = JID.new(jid_str)
-    
+
     @mq = MQ.new
-    @mq.prefetch PrefetchCount 
+    @mq.prefetch PrefetchCount
   end
 
   def start
     @client = Client.new(@jid)
-    
+
     if !connect
       return stop
     end
-    
+
     handle_exceptions
     receive_messages
     receive_subscriptions
     subscribe_queue
     keep_me_alive
   end
-  
+
   def connect
     begin
       server = @channel.configuration[:server].blank? ? nil : @channel.configuration[:server]
       @client.connect server, @channel.configuration[:port]
       @client.auth @channel.configuration[:password]
-      
+
       presence = Presence.new.set_show(:chat)
-      presence.set_status @channel.configuration[:status] if @channel.configuration[:status].present?      
+      presence.set_status @channel.configuration[:status] if @channel.configuration[:status].present?
       @client.send presence
       true
-    rescue => ex
-      alert_msg = ex.to_s
-      
-      Rails.logger.debug "Can't connect: #{alert_msg}"
+    rescue Exception => ex
+      alert_msg = "#{ex} #{ex.backtrace}"
+
       @channel.alert alert_msg
-    
+
       @channel.enabled = false
       @channel.save!
-            
+
       false
     end
   end
-  
+
   def send_message(id, from, to, subject, body)
     Rails.logger.debug "Sending message with id: '#{id}', from: '#{from}', to: '#{to}', subject: '#{subject}', body: '#{body}'"
-     
+
     msg = Message.new
     msg.id = id
     #msg.from = from
@@ -73,61 +72,61 @@ class XmppService < Service
 
     @client.send msg
   end
-  
+
   def stop
     @client.close
     @mq.close
     EM.stop_event_loop
   end
-  
+
   def receive_messages
     @client.add_message_callback do |msg|
       Rails.logger.debug "Receiving message #{msg}"
-      
+
       # Sometimes a nil msg arrives...
       next unless msg
-      
+
       if msg.type == :error
         ao = AOMessage.find_by_id msg.id
         if ao
           ao.state = 'failed'
           ao.save!
-          
+
           @channel.logger.exception_in_channel_and_ao_message @channel, ao, "Code #{msg.error.code} - #{msg.error.text}"
         else
           Rails.logger.debug "Received error message: #{msg}"
         end
         next
       end
-      
-      # The body might be empty when receiving "composing" messages 
+
+      # The body might be empty when receiving "composing" messages
       next unless msg.body.present?
-      
+
       at = ATMessage.new
       at.channel_relative_id = msg.id
       at.from = msg.from.bare.to_s.with_protocol 'xmpp'
       at.to = msg.to.bare.to_s.with_protocol 'xmpp'
       at.subject = msg.subject
       at.body = msg.body
-      
+
       @channel.route_at at
     end
   end
-  
+
   def receive_subscriptions
     @roster = Roster::Helper.new(@client)
     @roster.add_subscription_request_callback do |item, pres|
-      @channel.logger.info :message => "Accepting subscription from #{pres.from}", :application_id => @channel.application_id, :channel_id => @channel.id 
-    
-      # we accept everyone 
+      @channel.logger.info :message => "Accepting subscription from #{pres.from}", :application_id => @channel.application_id, :channel_id => @channel.id
+
+      # we accept everyone
       @roster.accept_subscription(pres.from)
- 
-      # Now it's our turn to send a subscription request  
-      presence = Presence.new.set_type(:subscribe).set_to(pres.from)  
+
+      # Now it's our turn to send a subscription request
+      presence = Presence.new.set_type(:subscribe).set_to(pres.from)
       @client.send presence
     end
   end
-  
+
   def subscribe_queue
     Queues.subscribe_ao(@channel, @mq) do |header, job|
       Rails.logger.debug "Executing job #{job}"
@@ -139,7 +138,7 @@ class XmppService < Service
       end
     end
   end
-  
+
   def handle_exceptions
     @client.on_exception do |ex|
       # TODO do something else...
@@ -151,7 +150,7 @@ class XmppService < Service
       end
     end
   end
-  
+
   def keep_me_alive
     EM.add_periodic_timer(1) do
       # Nothing
