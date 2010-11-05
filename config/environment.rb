2a6de089184ce36f0c572fc5ea0ca48fe76d128b
Juan Wajnerman <juan.wajnerman@gmail.com>
2010-11-05 13:04:55 -0300

    Added JSON as gem dependency

--- config/environment.rb
+++ config/environment.rb
@@ -36,6 +36,7 @@ Rails::Initializer.run do |config|
   config.gem 'daemon_controller'
   config.gem 'ruby-cache', :lib => 'cache'
   config.gem 'after_commit'
+  config.gem 'json'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
