32961f46df08f58e1327a67fbadb6cf50a3c2643
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-11-09 17:09:26 -0300

    Shortened code in send xmpp message job

--- app/models/xmpp/send_xmpp_message_job.rb
+++ app/models/xmpp/send_xmpp_message_job.rb
@@ -6,25 +6,20 @@ class SendXmppMessageJob
     @channel_id = channel_id
     @message_id = message_id
   end
-  
+
   def perform(delegate)
     account = Account.find_by_id @account_id
     channel = account.find_channel @channel_id
     msg = AOMessage.find @message_id
-    
-    from = msg.from.without_protocol
-    to = msg.to.without_protocol
-    subject = msg.subject
-    body = msg.body
-    
+
     begin
-      delegate.send_message(msg.id, from, to, subject, body)
+      delegate.send_message(msg.id, msg.from.without_protocol, msg.to.without_protocol, msg.subject, msg.body)
       msg.send_succeeed account, channel
     rescue => e
       msg.send_failed account, channel, e
     end
   end
-  
+
   def to_s
     "<SendXmppMessageJob:#{@message_id}>"
   end
