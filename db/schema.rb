9202a206005709be5b40d42cc7812b76f893acbf
Ary Borenszweig <aborenszweig@manas.com.ar>
2010-11-23 14:43:01 -0300

    Added migration to fix service channels

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101115155232) do
+ActiveRecord::Schema.define(:version => 20101123173500) do
 
   create_table "account_logs", :force => true do |t|
     t.integer  "account_id"
