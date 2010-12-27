d84b0d65691bcdee031ef3584138edc34bbb82de
Juan Wajnerman <juan.wajnerman@gmail.com>
2010-12-27 17:17:22 -0300

    Support SMTP without authentication

--- app/models/smtp/smtp_channel_handler.rb
+++ app/models/smtp/smtp_channel_handler.rb
@@ -6,7 +6,7 @@ class SmtpChannelHandler < GenericChannelHandler
   end
 
   def check_valid
-    check_config_not_blank :host, :user, :password
+    check_config_not_blank :host
     check_config_port
   end
 
