fe3e2b96ecb63d833dbb0f899383a6e823d06826
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-01-31 13:18:58 -0300

    Removed old class

--- app/models/callback/send_post_callback_message_job.rb
+++
@@ -1,4 +0,0 @@
-# Kept for backwards compatibility. Was replaced by SendInterfaceCallbackJob.
-# Will be removed in a future version.
-class SendPostCallbackMessageJob < SendInterfaceCallbackJob
-end
