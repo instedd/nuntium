b7d1cd6c78e9de6ebf7a24fbb35384bdf074885e
ary@esperanto.org.ar <ary@esperanto.org.ar>
2011-02-06 11:47:56 -0300

    Expand shortened texts on click

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,17 @@
+$(function() {
+  // This is to expand shortened text in tables
+  $("td").live('click', function() {
+    spans = $(this).children("span");
+    if (spans.length > 0) {
+      $span = $(spans[0]);
+      if ($span && $span.attr('title')) {
+        $span.text($span.attr('title'));
+        $span.attr('title', '');
+      }
+    }
+  });
+});
+
 function $n(id) {
   return document.getElementsByName(id);
 }
