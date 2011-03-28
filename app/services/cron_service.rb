314492b3f8e87769184de4136387d5494e1b9949
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-03-28 12:40:46 -0300

    Cleaned up cron service

--- app/services/cron_service.rb
+++ app/services/cron_service.rb
@@ -1,15 +1,15 @@
 module CronDaemonRun
-    
+
   # Gets tasks to run, enqueues a job for each of them and sets next run
   def cron_run
     to_run = CronTask.to_run
     to_run.each { |task| enqueue task }
-    rescue => err
-      Rails.logger.error "Error running scheduler: #{err}"
-    else
-      Rails.logger.info "Scheduler executed successfully enqueuing #{to_run.size} task(s)."
-    ensure
-      CronTask.set_next_run(to_run) unless to_run.nil?
+  rescue => err
+    Rails.logger.error "Error running scheduler: #{err}"
+  else
+    Rails.logger.info "Scheduler executed successfully enqueuing #{to_run.size} task(s)."
+  ensure
+    CronTask.set_next_run to_run if to_run
   end
 
   # Enqueue a descriptor for the specified task
@@ -21,16 +21,14 @@ module CronDaemonRun
 end
 
 class CronService < Service
-
   include CronDaemonRun
-  
+
   def initialize
     super
     Queues.bind_cron_tasks
   end
-  
+
   loop_with_sleep(20) do
     cron_run
   end
-
 end
