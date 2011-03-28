4e2366218a398dfcd79f168559936ecbcc4dbcce
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-03-28 12:38:46 -0300

    Refactored scheduled jobs service

--- app/models/scheduled_job.rb
+++ app/models/scheduled_job.rb
@@ -1,6 +1,10 @@
 class ScheduledJob < ActiveRecord::Base
   before_save :serialize_job
 
+  def self.due_to_run
+    all(:conditions => ['run_at <= ?', Time.now.utc])
+  end
+
   def perform
     handler = self.job.deserialize_job
     handler.perform
--- app/services/scheduled_jobs_service.rb
+++ app/services/scheduled_jobs_service.rb
@@ -6,15 +6,14 @@ class ScheduledJobsService < Service
   def execute_once
     Rails.logger.debug "Executing once..."
 
-    jobs = ScheduledJob.all(:conditions => ['run_at <= ?', Time.now.utc])
-    jobs.each do |job|
-      begin
-        job.perform
-      rescue Exception => e
-        Rails.logger.error "#{e.message}: #{e.backtrace}"
-      else
-        job.destroy
-      end
-    end
+    ScheduledJob.due_to_run.each { |job| perform_and_destroy job }
+  end
+
+  def perform_and_destroy(job)
+    job.perform
+  rescue Exception => e
+    Rails.logger.error "#{e.message}: #{e.backtrace}"
+  else
+    job.destroy
   end
 end
