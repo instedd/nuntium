974be0bf1487fcc56a93bab298a82aab4204e851
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-05-03 15:47:01 +0800

    /api/send_ao now can also accept json and xml in their body
    --HG--
    extra : rebase_source : 20aeea3ef40486007938ff13f77f3480a00532c0

--- app/controllers/send_ao_controller.rb
+++ app/controllers/send_ao_controller.rb
@@ -1,14 +1,43 @@
 class SendAoController < ApplicationAuthenticatedController
 
-  # GET /:account_name/:application_name/send_ao
+  # GET /:account_name/:application_name/send_ao.:format
   def create
+    case params[:format]
+    when nil
+      create_single
+    when 'json'
+      create_many_json
+    when 'xml'
+      create_many_xml
+    end
+  end
+
+  private
+
+  def create_single
     msg = AOMessage.from_hash params
-    msg.account_id = @account.id
-    @application.route_ao msg, 'http'
-    
+    route msg
+
     response.headers['X-Nuntium-Id'] = msg.id.to_s
     response.headers['X-Nuntium-Guid'] = msg.guid.to_s
     head msg.state == 'failed' ? :bad_request : :ok
+  end
+
+  def create_many_json
+    data = request.POST.present? ? request.POST : request.raw_post
+    AOMessage.from_json(data) { |msg| route msg }
+    head :ok
+  end
+
+  def create_many_xml
+    data = request.POST.present? ? request.POST : request.raw_post
+    AOMessage.parse_xml(data) { |msg| route msg }
+    head :ok
+  end
+
+  def route(msg)
+    msg.account_id = @account.id
+    @application.route_ao msg, 'http'
   end
 
 end
