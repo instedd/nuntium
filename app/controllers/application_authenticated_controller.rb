bbd01267fc7582736111ca8e8b75a93b3d7a42ed
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-09-06 15:14:53 -0300

    Backed out changeset 8e72b19617aa
    --HG--
    branch : v3

--- app/controllers/application_authenticated_controller.rb
+++ app/controllers/application_authenticated_controller.rb
@@ -1,8 +1,21 @@
 module ApplicationAuthenticatedController
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      @account, @application = Account.authenticate username, password, :only_application => true
-      @account && @application && @account.name == params[:account_name] && @application.name == params[:application_name]
+      success = false
+      account_name, app_name = username.split('/')
+      app_name, account_name = username.split('@') if app_name.nil?
+
+      if account_name == params[:account_name] and app_name == params[:application_name]
+        @account = Account.find_by_id_or_name account_name
+        if @account
+          @application = @account.applications.find_by_name app_name
+          if @application and @application.authenticate password
+            @application.account = @account
+            success = true
+          end
+        end
+      end
+      success
     end
   end
 end
