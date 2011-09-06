8d8568d6beba60520e37f9709f00f513b3d1d7eb
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-09-06 12:19:58 -0300

    Authentication logic is now in only one place
    --HG--
    branch : v3

--- app/controllers/api/api_authenticated_controller.rb
+++ app/controllers/api/api_authenticated_controller.rb
@@ -4,25 +4,8 @@ class ApiAuthenticatedController < ApplicationController
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      success = false
-      account_name, app_name = username.split '/'
-      app_name, account_name = username.split '@' if app_name.nil?
-      if account_name && app_name
-        @account = Account.find_by_id_or_name account_name
-        if @account
-          @application = @account.applications.find_by_name app_name
-          if @application && @application.authenticate(password)
-            @application.account = @account
-            success = true
-          end
-        end
-      else
-        @account = Account.find_by_id_or_name username
-        if @account && @account.authenticate(password)
-          success = true
-        end
-      end
-      success
+      @account, @application = Account.authenticate username, password
+      @account
     end
   end
 
