79f671404a2074898367dbc526a40b6ba5552608
Juan Wajnerman <jwajnerman@manas.com.ar>
2011-12-21 19:17:17 -0300

    Renamed pid files to avoid clashings in 'daemons' library
    --HG--
    extra : rebase_source : 64f04827b161a3b56106d343c8f09b2e0617645a

--- lib/services/generic_ctl.rb
+++ lib/services/generic_ctl.rb
@@ -1,8 +1,8 @@
-def run(script, id = nil) 
+def run(script, id = nil)
   require 'rubygems'
   require 'daemons'
   app_name = script
-  app_name += ".#{id}" if id 
+  app_name += ".#{id}." if id
   Daemons.run(
     File.join(File.dirname(__FILE__), "#{script}.rb"),
     :app_name => app_name,
