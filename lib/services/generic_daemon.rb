e4ebe57a4234ccd1421b906e24251b73a822e7ab
David Wilkie <dwilkie@gmail.com>
2012-05-02 13:15:10 +0700

    Backed out changeset 6df2f9cf6f15

--- lib/services/generic_daemon.rb
+++ lib/services/generic_daemon.rb
@@ -6,8 +6,6 @@ def start_service(log_name)
   require File.expand_path('../../../config/environment',  __FILE__)
 
   yield
-rescue SystemExit
-  Rails.logger.info "Stopping service normally"
 rescue Exception => err
   File.open($log_path, 'a') { |f| f.write "Daemon failure: #{err} #{err.backtrace}\n" }
 end
