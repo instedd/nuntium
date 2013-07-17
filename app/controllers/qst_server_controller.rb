9ac87d6e4ff012100fa84604485f10d21cd9a099
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-07-17 16:11:40 -0300

    Fixed QstServerController#get_last_id removing check on HEAD requests

--- app/controllers/qst_server_controller.rb
+++ app/controllers/qst_server_controller.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2009-2012, InSTEDD
-# 
+#
 # This file is part of Nuntium.
-# 
+#
 # Nuntium is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Nuntium is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Nuntium.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -44,7 +44,7 @@ class QstServerController < ApplicationController
 
   # HEAD /qst/:account_id/incoming
   def get_last_id
-    return head(:not_found) unless request.head?
+    # return head(:not_found) unless request.head?
     msg = AtMessage.select(:guid).where(:account_id => @account.id, :channel_id => @channel.id).order(:timestamp).last
     etag = msg.nil? ? nil : msg.guid
     head :ok, 'Etag' => etag
