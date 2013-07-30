83f00ed74c508dc870d46fb061ab3de6c7c55777
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-07-29 23:29:13 -0300

    Fix error with api/countries/XX.xml

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2009-2012, InSTEDD
-# 
+#
 # This file is part of Nuntium.
-# 
+#
 # Nuntium is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Nuntium is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Nuntium.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -50,7 +50,7 @@ class Country < ActiveRecord::Base
 
   def to_xml(options = {})
     options[:indent] ||= 2
-    xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
+    xml = options[:builder] ||= ::Builder::XmlMarkup.new(:indent => options[:indent])
     xml.instruct! unless options[:skip_instruct]
     xml.country :name => name, :iso2 => iso2, :iso3 => iso3, :phone_prefix => phone_prefix
   end
--- test/functional/api_country_controller_test.rb
+++ test/functional/api_country_controller_test.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2009-2012, InSTEDD
-# 
+#
 # This file is part of Nuntium.
-# 
+#
 # Nuntium is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Nuntium is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Nuntium.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -46,6 +46,15 @@ class ApiCountryControllerTest < ActionController::TestCase
     end
     ['id', 'created_at', 'updated_at'].each do |excluded|
       assert_false countries[0].has_key? excluded
+    end
+  end
+
+  test "show xml by iso2" do
+    get :show, :format => 'xml', :iso => 'ar'
+    assert_response :ok
+
+    @attributes.each do |key, value|
+      assert_select "country[#{key}=?]", value
     end
   end
 
