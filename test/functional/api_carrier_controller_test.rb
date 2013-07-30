4a9a3b748b978f8d67c482166a335c807bbb5693
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-07-29 23:36:39 -0300

    Fix error with api/carriers/GUID.xml

--- test/functional/api_carrier_controller_test.rb
+++ test/functional/api_carrier_controller_test.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2009-2012, InSTEDD
-# 
+#
 # This file is part of Nuntium.
-# 
+#
 # Nuntium is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Nuntium is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Nuntium.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -51,6 +51,15 @@ class ApiCarrierControllerTest < ActionController::TestCase
     ['id', 'country', 'country_id', 'clickatell_name', 'prefixes', 'created_at', 'updated_at'].each do |excluded|
       assert_false carriers[0].has_key? excluded
     end
+  end
+
+  test "show xml" do
+    get :show, :format => 'xml', :guid => @carrier.guid
+    assert_response :ok
+
+    assert_select "carrier[name=?]", @carrier.name
+    assert_select "carrier[guid=?]", @carrier.guid
+    assert_select "carrier[country_iso2=?]", @country.iso2
   end
 
   test "index xml for country code iso2" do
