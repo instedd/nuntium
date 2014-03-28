999de3110984b32cfdf6ae1e093106a84e03afc4
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-03-28 11:22:31 -0300

    Upgrade Rails to 3.2.17

--- test/unit/receive_pop3_message_job_test.rb
+++ test/unit/receive_pop3_message_job_test.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2009-2012, InSTEDD
-# 
+#
 # This file is part of Nuntium.
-# 
+#
 # Nuntium is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Nuntium is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Nuntium.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -173,12 +173,10 @@ class ReceivePop3MessageJobTest < ActiveSupport::TestCase
     msg << "Content-Type: text/plain; charset=ISO-8859-1\n"
     msg << "\n"
     msg << "Hola\n"
-    msg << "\n"
     msg << "--bcaec53f90c1079a6d04ad75afdb\n"
     msg << "Content-Type: text/html; charset=ISO-8859-1\n"
     msg << "\n"
     msg << "Hola\n"
-    msg << "\n"
     msg << "--bcaec53f90c1079a6d04ad75afdb--\n"
 
     mail = mock('Net::POPMail')
