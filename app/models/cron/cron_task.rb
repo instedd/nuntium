2f7c5e765d68d4b6193c8ee5357c09cbf1940fc1
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-08-16 13:07:01 -0300

    Do not sort cron tasks (MySQL 5.5 would try to sort them in memory)

--- app/models/cron/cron_task.rb
+++ app/models/cron/cron_task.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2009-2012, InSTEDD
-# 
+#
 # This file is part of Nuntium.
-# 
+#
 # Nuntium is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Nuntium is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Nuntium.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -78,7 +78,7 @@ class CronTask < ActiveRecord::Base
 
   # Returns all tasks with next run less than current time
   def self.to_run
-    where('next_run is null or next_run <= ?', Time.now.utc).order('next_run ASC').all
+    where('next_run is null or next_run <= ?', Time.now.utc).all
   end
 
   # For every task set next run time to current time plus interval
