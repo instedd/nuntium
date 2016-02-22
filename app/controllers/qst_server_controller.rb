af78e51346fb452b30a59418d3f0e5ddf2783a4f
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-02-22 11:46:54 -0300

    QST server: don't use `update_all` when marking QST messages as delivered, so callbacks get triggered. Related to instedd/pollit#45

--- app/controllers/qst_server_controller.rb
+++ app/controllers/qst_server_controller.rb
@@ -79,7 +79,11 @@ class QstServerController < ApplicationController
         # Mark messsages as delivered
         outs = @channel.qst_outgoing_messages.select(:ao_message_id).where 'ao_message_id <= ?', last.id
         outs.each do |out|
-          AoMessage.where(:id => out.ao_message_id, :state => 'queued').update_all "state = 'delivered'"
+          # Don't use update_all because we want save callbacks like delivery ack
+          AoMessage.where(:id => out.ao_message_id, :state => 'queued').each do |ao_message|
+            ao_message.state = 'delivered'
+            ao_message.save!
+          end
         end
 
         # Delete previous messages in qst including it
