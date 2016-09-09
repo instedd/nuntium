f6a6e510aa27f0c8a56a070be0e1f11b647dd271
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-09-09 11:54:54 -0300

    Some fixes in pigeonui.js:  - accept `options` with `callback` and `accessToken`  - expose the api through a `pigeon` global object  - render the iframe with a solid white background color

--- app/assets/javascripts/pigeonui.js
+++ app/assets/javascripts/pigeonui.js
@@ -14,43 +14,46 @@
     }
   }
 
-  win.addChannel = function(callback) {
-    var overlay = $("<div>")
-      .css({
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-        zIndex: 10000
-      })
-      .click(function() {
-        callback(null);
-        document.body.removeChild(overlay);
-      })[0];
-    document.body.appendChild(overlay);
+  win.pigeon = {
+    addChannel: function(options) {
+      var overlay = $("<div>")
+        .css({
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          zIndex: 10000
+        })
+        .click(function() {
+          options.callback(null);
+          document.body.removeChild(overlay);
+        })[0];
+      document.body.appendChild(overlay);
 
-    var frame = $("<iframe>")
-      .attr("src", nuntiumScriptSrc + "/pigeon/new")
-      .css({
-        position: "absolute",
-        border: "0px",
-        top: "25%",
-        left: "25%",
-        height: "50%",
-        width: "50%"
-      })[0]
-    overlay.appendChild(frame)
+      var frame = $("<iframe>")
+        .attr("src", nuntiumScriptSrc + "/pigeon/new?access_token=" + options.accessToken)
+        .css({
+          backgroundColor: "white",
+          position: "absolute",
+          border: "0px",
+          top: "25%",
+          left: "25%",
+          height: "50%",
+          width: "50%"
+        })[0]
+      overlay.appendChild(frame)
 
-    var listener = function(event) {
-      win.removeEventListener("message", listener);
-      if (event.source == frame.contentWindow && event.origin == nuntiumScriptSrc) {
-        callback(event.data);
-        document.body.removeChild(overlay);
+      var listener = function(event) {
+        win.removeEventListener("message", listener);
+        if (event.source == frame.contentWindow && event.origin == nuntiumScriptSrc) {
+          options.callback(event.data);
+          document.body.removeChild(overlay);
+        }
       }
-    }
 
-    win.addEventListener("message", listener, false);
+      win.addEventListener("message", listener, false);
+    }
   }
 })(window);
