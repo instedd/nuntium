7c41c25767b4a5e617ac6a53e7dfae4e47401a5f
Adam Preston <adam.m.preston@gmail.com>
2017-03-07 17:19:34 -0700

    added send message job implementation

---
+++ app/models/messenger/send_messenger_message_job.rb
@@ -0,0 +1,37 @@
+#!ruby
+class SendMyKindMessageJob < SendMessageJob
+  def managed_perform
+    # Here you have:
+    # @msg: the AO message to be sent. Most notably, you will use
+    #       the from, to, subject and body accessors. You also have
+    #       the subject_and_body accessor that combines both subject
+    #       and body with a dash.
+    # @account
+    # @channel
+    # @config: convenience variable that holds the channel's configuration
+
+    # Do whatever you want with the message...
+    # For example, issue an HTTP get with it:
+    data = { 
+      :from => @msg.from,
+      :to => @msg.to, 
+      :text => @msg.subject_and_body
+    }
+    res = RestClient::Resource.new(@channel.url).post data
+    netres = res.net_http_res
+    case netres
+    when Net::HTTPSuccess, Net::HTTPRedirection
+      # Message was sent, must return "true"
+      return true
+    when Net::HTTPUnauthorized
+      # Message was not sent, but it's a channel's problem.
+      # Must raise a PermanentException
+      raise PermamentException.new(Exception.new "unauthorized")
+    else
+      # Something else went wrong, probably something's wrong with
+      # the message or a temporary error...
+      # Must raise an Exception
+      raise Exception.new("Something's wrong...")
+    end
+  end
+end
