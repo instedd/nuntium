95eba5f9741a44bbc5358c0abcb5c2c1820ba03a
Juan F. Blandini <jblandini@manas.com.ar>
2018-06-22 11:50:29 -0300

    Handling all the statusCode values Storing the network_code when it comes on the answer of the delivery report Related to #36 Africa's talking channel

--- app/controllers/africas_talking_controller.rb
+++ app/controllers/africas_talking_controller.rb
@@ -61,16 +61,71 @@ class AfricasTalkingController < ApplicationController
     when "Failed", "Rejected"
       ao.state = "failed"
       ao.custom_attributes["africas_talking_failure_reason"] = params[:failureReason] if params[:failureReason]
-    else
-      ao.state = "failed"
+    # when "Submitted", "Buffered"
+    #   ao.state = "queued"
+    # else
+    #   ao.state = "failed"
     end
 
     account.logger.info :channel_id => channel.id, :ao_message_id => ao.id,
       :message => "Recieved delivery notification with status #{status.inspect} (retried #{retry_count} times)"
 
+    ao.custom_attributes["africas_talking_network_code"] = translate_network_code(params[:networkCode]) if params[:networkCode]
     ao.custom_attributes["africas_talking_retries"] = retry_count if retry_count
     ao.save!
 
     render text: "Accepted"
   end
+
+  def translate_network_code network_code
+    case network_code.to_i
+      when 62120
+        "Airtel Nigeria"
+      when 62130
+        "MTN Nigeria"
+      when 62150
+        "Glo Nigeria"
+      when 62160
+        "Etisalat Nigeria"
+      when 63510
+        "MTN Rwanda"
+      when 63513
+        "Tigo Rwanda"
+      when 63514
+        "Airtel Rwanda"
+      when 63902
+        "Safaricom"
+      when 63903
+        "Airtel Kenya"
+      when 63907
+        "Orange Kenya"
+      when 63999
+        "Equitel Kenya"
+      when 64002
+        "Tigo Tanzania"
+      when 64003
+        "Zantel Tanzania"
+      when 64004
+        "Vodacom Tanzania"
+      when 64005
+        "Airtel Tanzania"
+      when 64007
+        "TTCL Tanzania"
+      when 64009
+        "Halotel Tanzania"
+      when 64101
+        "Airtel Uganda"
+      when 64110
+        "MTN Uganda"
+      when 64111
+        "UTL Uganda"
+      when 64114
+        "Africell Uganda"
+      when 65001
+        "TNM Malawi"
+      when 65010
+        "Airtel Malawi"
+    end
+  end
+
 end
--- app/models/africas_talking/send_africas_talking_message_job.rb
+++ app/models/africas_talking/send_africas_talking_message_job.rb
@@ -16,6 +16,21 @@
 # along with Nuntium.  If not, see <http://www.gnu.org/licenses/>.
 require_relative './AfricasTalkingGateway'
 
+# StatusCode values:
+# 100: Processed
+# 101: Sent
+# 102: Queued
+# 401: RiskHold
+# 402: InvalidSenderId
+# 403: InvalidPhoneNumber
+# 404: UnsupportedNumberType
+# 405: InsufficientBalance
+# 406: UserInBlackList
+# 407: CouldNotRoute
+# 500: InternalServerError
+# 501: GatewayError
+# 502: RejectedByGateway
+
 class SendAfricasTalkingMessageJob < SendMessageJob
   def managed_perform
     gateway = ::AfricasTalkingGateway.new(@config[:username], @config[:api_key], @config[:use_sandbox] == "1" ? "sandbox" : nil)
@@ -26,19 +41,37 @@ class SendAfricasTalkingMessageJob < SendMessageJob
       reports = gateway.sendMessage(destination_number, @msg.body, @config[:shortcode])
 
       report = reports[0]
-
-      if (report.status != "Success")
-        raise MessageException.new(Exception.new(reports[0].status))
-        false
-      else
-        @msg.channel_relative_id = report.messageId if report.messageId
-        @msg.custom_attributes["africas_taliking_status"] = report.status if report.status
-        @msg.custom_attributes["africas_taliking_credits_cost"] = report.cost if report.cost
-        true
+      case report.statusCode
+        when 100, 101, 102
+          success_delivery(report)
+        when 401, 403, 404, 406, 502
+          raise MessageException.new(Exception.new(report.status))
+          false
+        when 405, 407, 500, 501
+          raise "Temporal problem #{report.status}"
+          false
+        when 402
+          raise PermanentException.new(Exception.new(report.status))
+          false
+        else
+          # We are probably using sandbox, but this is also useful in case we are in production mode and the API doesn't send the statusCode
+          if (report.status != "Success")
+            raise MessageException.new(Exception.new(report.status))
+            false
+          else
+            success_delivery(report)
+          end
       end
     rescue AfricasTalkingGatewayException => ex
       puts 'Encountered an error: ' + ex.message
       false
     end
+  end
+
+  def success_delivery report
+    @msg.channel_relative_id = report.messageId if report.messageId
+    @msg.custom_attributes["africas_taliking_status"] = report.status if report.status
+    @msg.custom_attributes["africas_taliking_credits_cost"] = report.cost if report.cost
+    true
   end
 end
