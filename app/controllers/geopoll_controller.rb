77236f7fd71a0e2eefeea07f9866b069d612cf0d
Matías García Isaía <mgarcia@manas.tech>
2022-11-23 19:35:07 -0300

    Detect GeoPoll AT's to: number (#88)
    * Securely check GeoPoll incoming signature
    
    This avoids timing attacks
    
    * Log unknown GeoPoll requests parameters
    
    To avoid losing data upon API changes
    
    See #76
    
    * Show GeoPoll channels in Interactions tab
    
    * Detect GeoPoll AT's to: numbers
    
    Fixes #76

--- app/controllers/geopoll_controller.rb
+++ app/controllers/geopoll_controller.rb
@@ -20,7 +20,7 @@ require 'digest/md5'
 class GeopollController < ApplicationController
   skip_filter :check_login
 
-  # POST /:account_name/:channel_name/:secret_token/geopoll/incoming
+  # POST /:account_name/:channel_name/geopoll/incoming
   def incoming
     account = Account.find_by_id_or_name(params[:account_name])
     channel = account.geopoll_channels.find_by_name(params[:channel_name])
@@ -28,17 +28,24 @@ class GeopollController < ApplicationController
     identifier = params[:Identifier]
     signature = Digest::MD5.hexdigest(auth_token + identifier)
 
-    if signature != params[:Signature]
+    if !(ActiveSupport::SecurityUtils.secure_compare params[:Signature], signature)
       return render text: "Error", status: :unauthorized
     end
 
+    unknown_params = params.except(
+      'Identifier', 'Signature', 'SourceAddress', 'TargetAddress', 'MessageText', # GeoPoll API specification
+      'account_name', 'channel_name', 'controller', 'action' # Rails-generated parameters
+    )
+
     msg = AtMessage.new
     msg.from = "sms://#{params[:SourceAddress]}"
-    msg.to   = "sms://#{channel.configuration[:from]}"
+    msg.to   = "sms://#{params[:TargetAddress]}"
     msg.body = params[:MessageText]
     msg.channel_relative_id = params[:Identifier]
     account.route_at msg, channel
 
+    channel.logger.warning :channel_id => channel.id, :at_message_id => msg.id, :message => "Received unknown parameters for AT #{msg.id}: #{unknown_params.to_json}" unless unknown_params.empty?
+    
     render text: "Accepted"
   end
 
@@ -50,6 +57,11 @@ class GeopollController < ApplicationController
     unless ao
       return render text: "Error", status: :not_found
     end
+
+    unknown_params = params.except(
+      'MessageId', 'Status', # GeoPoll API specification
+      'account_name', 'channel_name', 'controller', 'action' # Rails-generated parameters
+    )
 
     status = params[:Status]
     case status
@@ -63,8 +75,10 @@ class GeopollController < ApplicationController
     account.logger.info :channel_id => channel.id, :ao_message_id => ao.id,
       :message => "Recieved delivery notification with status #{status.inspect}"
 
-     ao.custom_attributes[:geopoll_status] = status if status
-     ao.save!
+    channel.logger.warning :channel_id => channel.id, :at_message_id => msg.id, :message => "Received unknown parameters for AO #{msg.id} Status: #{unknown_params.to_json}" unless unknown_params.empty?
+
+    ao.custom_attributes[:geopoll_status] = status if status
+    ao.save!
 
     render text: "Accepted"
   end
