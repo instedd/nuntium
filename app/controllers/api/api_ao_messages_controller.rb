8e775173d58ae1003422fc4f414641975eda58e4
Julien Portalier <julien@portalier.com>
2023-05-05 14:58:21 +0200

    Add the api_ao_messages#index action
    Exposes the ao_messages#get_ao to the OAuth 2.0 API. Requires the
    `token` attribute to be defined.

--- app/controllers/api/api_ao_messages_controller.rb
+++ app/controllers/api/api_ao_messages_controller.rb
@@ -2,6 +2,12 @@ class ApiAoMessagesController < ApiAuthenticatedController
   before_filter :require_account_and_application!
   include AoMessageCreateCommon
 
+  rescue_from(KeyError) { head :bad_request }
+
+  def index
+    render :json => AoMessage.find_all_by_application_id_and_token(@application.id, params.fetch(:token))
+  end
+
   def create
     create_from_request
   end
---
+++ test/functional/api_ao_messages_controller_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class ApiAoMessagesControllerTest < ActionController::TestCase
+  def setup
+    @account = Account.make :password => 'secret'
+    @application = Application.make :account => @account, :password => 'secret'
+  end
+
+  def authorize
+    @request.env['HTTP_AUTHORIZATION'] = http_auth("#{@account.name}/#{@application.name}", 'secret')
+  end
+
+  def create_test_ao_messages(token)
+    @ao_msg1 = AoMessage.create! :account_id => @account.id, :application_id => @application.id, :state => 'pending', :body => 'one', :to => 'sms://1', token: token
+    @ao_msg2 = AoMessage.create! :account_id => @account.id, :application_id => @application.id, :state => 'pending', :body => 'one', :to => 'sms://1', token: token
+    @ao_msg3 = AoMessage.create! :account_id => @account.id, :application_id => @application.id, :state => 'pending', :body => 'two', :to => 'sms://1'
+  end
+
+  test "index" do
+    create_test_ao_messages(token = Guid.new.to_s)
+    authorize
+    get :index, format: "json", token: token
+    assert_response :ok
+
+    assert_equal [
+      {
+        "to" => "sms://1",
+        "body" => "one",
+        "guid" => @ao_msg1.guid,
+        "channel" => nil,
+        "channel_kind" => nil,
+        "state" => "pending"
+      },
+      {
+        "to" => "sms://1",
+        "body" => "one",
+        "guid" => @ao_msg2.guid,
+        "channel" => nil,
+        "channel_kind" => nil,
+        "state" => "pending"
+      }
+    ], JSON.parse(@response.body)
+  end
+
+  test "index requires the token param" do
+    authorize
+    get :index, format: "json"
+    assert_response :bad_request
+  end
+end
